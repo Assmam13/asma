251bb5a113b1de96641a10280118b65637361d2f
asma <asmabenaissa156@gmail.com>
2026-03-12 12:34:23 +0100

    feat: monnaies + carte Leaflet fusionnees

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,38 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { CarteComponent } from './carte/carte';
+import { HomeComponent }           from './home/home';
+import { LoginComponent }          from './login/login';
+import { RequestAccessComponent }  from './request-access/request-access';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password';
+import { MonnaiesComponent }       from './monnaies/monnaies';
+import { CarteComponent }          from './carte/carte';
 
 export const routes: Routes = [
-  { 
-    path: '', 
+  {
+    path: '',
     component: HomeComponent,
+    title: 'Moneta - Dashboard',
     children: [
-      { path: 'cartes', component: CarteComponent }
+      { path: 'cartes',   component: CarteComponent },    // carte de ton ami
+      { path: 'monnaies', component: MonnaiesComponent }  // tes monnaies
     ]
   },
-  { path: 'login', loadComponent: () => import('./login/login').then(m => m.LoginComponent) },
-  { path: '**', redirectTo: '' }
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Moneta - Connexion'
+  },
+  {
+    path: 'request',
+    component: RequestAccessComponent,
+    title: 'Moneta - Inscription'
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    title: 'Moneta - Mot de passe oublié'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
